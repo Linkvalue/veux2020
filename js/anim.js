650cbeadc43b2fd777cedcb92020f9a66560214d
Martin <martin.meurin@link-value.fr>
2020-01-06 18:26:06 +0100

    adaptive css

--- js/anim.js
+++
@@ -1,7 +0,0 @@
-var svgContainer = document.getElementById('handContainer');
-var animItem = bodymovin.loadAnimation({
-  wrapper: svgContainer,
-  animType: 'svg',
-  loop: true,
-  path: './data/bee-fiying.json'
-});
